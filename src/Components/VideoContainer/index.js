3aea5fb368c265230362be1f45e69df4d52db26c
Will Fallows <will@willfallows.com>
2017-11-09 00:04:18 -0500

    Added infinite scroll

--- src/Components/VideoContainer/index.js
+++ src/Components/VideoContainer/index.js
@@ -1,28 +1,76 @@
 import React, { Component } from 'react';
-import autobind from 'react-autobind';
-import {Grid, Row, Col, Image, Button, Thumbnail} from 'react-bootstrap';
+import {Grid, Row, Col} from 'react-bootstrap';
 import SurfVideo from '../SurfVideo';
+import autobind from 'react-autobind';
+import YoutubeService from '../../Services/youtube';
 
 
 export default class VideoContainer extends Component {
     constructor(props) {
         super(props);
-        console.log('Props: ' + JSON.stringify(this.props));
         autobind(this);
+
+        this.state = {
+            videos: [],
+            loading: false,
+            nextPageToken: null 
+          };
+        this._youtubeService = new YoutubeService();
     }
 
+    componentWillMount() {
+        this.setState({ loading: true });
+        this._loadVideos({})
+            .then((videoData) => {
+                this.setState({videos: this.state.videos.concat(videoData.videos), nextPageToken: videoData.nextPageToken, loading: false});
+            });
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.onScroll, false);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.onScroll, false);
+    }
+
+    onScroll() {
+        if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 200) && this.state.videos.length) {
+            if(this.state.loading){
+                return;
+            }
+
+            this.setState({isInfiniteLoading: true});
+            this._loadVideos({nextPageToken: this.state.nextPageToken})
+                .then((videoData) => {
+                    this.setState({videos: this.state.videos.concat(videoData.videos), nextPageToken: videoData.nextPageToken, loading: false});
+                });
+        }
+      }
+
     render() {
+        let videos = this.state.videos;
+
         return (
             <Grid>
-                <Row>
-                    <Col xs={6} md={4}>
-                        <pre>{JSON.stringify(this.props.videos) }</pre>
-                        {this.props.videos.map(video => (
-                            <SurfVideo video={video}/>
-                        ))}
-                    </Col>
-                </Row>
+                {  
+                    Object.keys(videos).map((videoIndex) => (
+                        <SurfVideo key={videoIndex} video={videos[videoIndex]}/>
+                    ))
+                }
             </Grid>
         )
+    }
+
+    _loadVideos(options) {
+        return new Promise((resolve, reject) => {
+            this._youtubeService.executeSearchQuery(options)
+                .then((data) => {
+                    return resolve(data);
+                })
+                .catch((err) => {
+                    return reject(err);
+                });
+        })
     }
 }
